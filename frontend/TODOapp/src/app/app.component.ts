5ecc1e72c937be25d70713abb8a9f320dbfaa555
Gabriel Andrade <gabriel.andradesx@gmail.com>
2024-04-27 13:36:00 -0300

    final?

--- frontend/TODOapp/src/app/app.component.ts
+++ frontend/TODOapp/src/app/app.component.ts
@@ -14,7 +14,7 @@ export class AppComponent {
   apiURL: string;
 
   constructor(private http: HttpClient) {
-    this.apiURL = 'http://localhost:3000';
+    this.apiURL = 'https://angular-branqs-api.vercel.app/';
     this.READ_tasks();
   }
 
